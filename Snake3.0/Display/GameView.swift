b87be0b93d1e1e3ddae86f438fc27a289bfb9a21
Rick Twohy <twohyjr@gmail.com>
2019-08-24 17:42:00 -0400

    Clearing the screen

--- Snake3.0/Display/GameView.swift
+++ Snake3.0/Display/GameView.swift
@@ -2,8 +2,16 @@ import MetalKit
 
 class GameView: MTKView {
 
+    var commandQueue: MTLCommandQueue!
+    
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        
+        self.device = MTLCreateSystemDefaultDevice()
+        
+        self.clearColor = MTLClearColor(red: 0.2, green: 0.5, blue: 0.3, alpha: 1.0)
+        
+        self.commandQueue = self.device?.makeCommandQueue()
     
         self.delegate = self
     }
@@ -17,7 +25,16 @@ extension GameView: MTKViewDelegate {
     }
     
     func draw(in view: MTKView) {
+        guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
         
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        
+        // Drawing
+        
+        renderCommandEncoder?.endEncoding()
+        commandBuffer?.present(view.currentDrawable!)
+        commandBuffer?.commit()
     }
     
 }
