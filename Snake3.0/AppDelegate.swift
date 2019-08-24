962045a72c55dc78f6d660fd0d7c5164a1bcd428
Rick Twohy <twohyjr@gmail.com>
2019-08-24 17:19:27 -0400

    Initial Commit

---
+++ Snake3.0/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Snake3.0
+//
+//  Created by Rick Twohy Jr on 8/24/19.
+//  Copyright © 2019 Tutorials. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
