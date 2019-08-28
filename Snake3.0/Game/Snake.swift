33af510480ac5d5ec487efffdb8bef38b13107d5
Rick Twohy <twohyjr@gmail.com>
2019-08-27 21:07:31 -0400

    Adding Body Parts

--- Snake3.0/Game/Snake.swift
+++ Snake3.0/Game/Snake.swift
@@ -3,10 +3,11 @@ import MetalKit
 class Snake: GameObject {
     
     private var _nextDirection = float3(1,0,0)
-    private var _timeTicked: Int = 0
-    private var _head: SnakeBody!
+    private var _timeTicked: Float = 0
+    private var _head: SnakeBody! = nil
+    private var _tail: SnakeBody! = nil
     var canTick: Bool {
-        if(_timeTicked > GameSettings.SnakeSpeed) {
+        if(_timeTicked > 60 / GameSettings.SnakeSpeed) {
             _timeTicked = 0
             return true
         }
@@ -18,19 +19,27 @@ class Snake: GameObject {
         super.init()
     
         addBody()
+        addBody()
+        addBody()
     }
     
     func addBody() {
-        _head = SnakeBody()
-        
-        addChild(_head)
-    }
-    
-    override func doUpdate() {
-        if(canTick){
-            _head.move((1 / GameSettings.GridCellsWide) * self._nextDirection)
+        var body: SnakeBody!
+        if(_head == nil) {
+            body = SnakeBody(nextDirection: self._nextDirection)
+            self._head = body
+        }else{
+            if(_tail.nextDirection == float3(1,0,0)) {
+                body = SnakeBody(nextDirection: _tail.nextDirection)
+                body.setPosition(_tail.getPosition() + -_tail.nextDirection / GameSettings.GridCellsWide)
+            }
         }
         
+        self._tail = body
+        addChild(body)
+    }
+    
+    private func getInput() {
         if(Keyboard.IsKeyPressed(.leftArrow)) {
             self._nextDirection = float3(-1,0,0)
         }
@@ -44,16 +53,43 @@ class Snake: GameObject {
             self._nextDirection = float3(0,-1,0)
         }
     }
+
+    override func doUpdate() {
+        getInput()
+        
+        if(canTick){
+            if(self._head.nextDirection != _nextDirection && _nextDirection != -self._head.nextDirection) {
+                _head.turn(_nextDirection)
+            }
+            for child in children {
+                if let snakeBody = child as? SnakeBody {
+                    snakeBody.slither()
+                }
+            }
+        }
+        
+        
+    }
     
 }
 
 class SnakeBody: GameObject {
     
-    override init() {
+    var nextDirection = float3(0,0,0)
+    init(nextDirection: float3) {
         super.init()
         
+        self.nextDirection = nextDirection
         self.setPositionZ(0.101)
         self.setScale(1 / GameSettings.GridCellsHigh * (1 - GameSettings.GridLinesWidth * 2))
+    }
+    
+    func turn(_ direction: float3) {
+        self.nextDirection = direction
+    }
+    
+    func slither() {
+        self.move((1 / GameSettings.GridCellsWide) * self.nextDirection)
     }
     
 }
